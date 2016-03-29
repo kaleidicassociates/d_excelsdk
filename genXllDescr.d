668fcfc5dbb016f29aa16e255d5f5d71f7a0804d
Stefan Koch <uplink.coder@googlemail.com>
2016-03-29 23:35:19 +0000

    start on methodic excel wrapping

---
+++ genXllDescr.d
@@ -0,0 +1,50 @@
+﻿module genXllDescr;
+import xlltypes;
+template descr(alias exportedFunction) {
+	enum descr = mixin(descr_!exportedFunction);
+	static assert(is(typeof(descr) == wstring[]));
+}
+
+wchar typeText(T)(T t) {
+	// are C and F the same ?
+	// F is modified in place
+	// C is not
+	static if (is(T == LxOper)) {
+
+	} else static if (is(T == double)) {
+		return "B"w;
+	} else static if (is(T == double*)) {
+		return "E"w;
+	} else static if (is(T == Boolean)) {
+		return "A"w;
+	} else static if (is(T == Boolean*)) {
+		return "L"w;
+	} else static if (is(T == const char*)) {
+		return "C"w;
+	} else static if (is(T == char*)) {
+		return "F"w;
+	}
+}
+
+string descr_(alias exportedFunction)() {
+	import std.traits;
+	static assert(hasUDA!(exportedFunction, Xll));
+
+
+
+	string result = "[";
+	/* Output looks like :
+	[ "Func1"w,                                     // Procedure
+		"UU"w,                                  // type_text
+		"Func1"w,                               // function_text
+		"Arg"w,                                 // argument_text
+		"1"w,                                   // macro_type
+		"Generic Add-In"w,                      // category
+		""w,                                    // shortcut_text
+		""w,                                    // help_topic
+		"Always returns the string 'Func1'"w,   // function_help
+		"Argument ignored"w                     // argument_help1
+	]
+*/
+	 
+}
---
+++ xllTypes.d
@@ -0,0 +1,14 @@
+﻿module xlltypes;
+
+struct Boolean {
+	short _value;
+	@property bool asBool() pure {
+		return _value != 0;
+	}
+
+	@property void asBool(bool val) pure {
+		val ? _value = 1 : _value = 0;
+	}
+
+	alias this asBool;
+}
