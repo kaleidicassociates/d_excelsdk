6e3d45331abf64901388f814c1154296aeb4988c
Atila Neves <aalvesne@cisco.com>
2016-10-25 16:55:32 +0100

    GenerateDllDef template mixin

--- example/myxll.d
+++ example/myxll.d
@@ -10,22 +10,4 @@ module example.myxll;
 
 import xlld;
 mixin(wrapAll!(__MODULE__, "xlld.test_d_funcs"));
-version(main) {
-    void main(string[] args) {
-
-        import std.stdio: File;
-        import std.exception: enforce;
-        import std.path: stripExtension;
-
-        enforce(args.length >= 2 && args.length <= 4,
-                "Usage: " ~ args[0] ~ " [file_name] <lib_name> <description>");
-
-        immutable fileName = args[1];
-        immutable libName = args.length > 2 ? args[2] : fileName.stripExtension ~ ".xll";
-        immutable description = args.length > 3 ? args[3] : "Simple D add-in to Excel";
-
-        auto file = File(fileName, "w");
-        foreach(stmt; dllDefFile!__MODULE__(libName, description).statements)
-            file.writeln(stmt.toString);
-    }
-}
+mixin GenerateDllDef;
--- source/xlld/traits.d
+++ source/xlld/traits.d
@@ -341,3 +341,26 @@ unittest {
         )
     );
 }
+
+
+mixin template GenerateDllDef(string module_ = __MODULE__) {
+    version(main) {
+        void main(string[] args) {
+
+            import std.stdio: File;
+            import std.exception: enforce;
+            import std.path: stripExtension;
+
+            enforce(args.length >= 2 && args.length <= 4,
+                    "Usage: " ~ args[0] ~ " [file_name] <lib_name> <description>");
+
+            immutable fileName = args[1];
+            immutable libName = args.length > 2 ? args[2] : fileName.stripExtension ~ ".xll";
+            immutable description = args.length > 3 ? args[3] : "Simple D add-in to Excel";
+
+            auto file = File(fileName, "w");
+            foreach(stmt; dllDefFile!module_(libName, description).statements)
+                file.writeln(stmt.toString);
+        }
+    }
+}
