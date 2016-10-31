58abb4cb08948b6d1f16cbf7fd369268596bbe33
Atila Neves <aalvesne@cisco.com>
2016-10-31 17:22:11 +0000

    Still works after removing a few flags

--- make32.bat
+++ make32.bat
@@ -4,10 +4,10 @@ rem Write the .def file automatically
 dub run -c def --nodeps -- myxll32.def || goto :error
 
 rem Build the XLL object file
-dmd -c -g -m32 -Isource -ofmyxll.obj example/myxll.d source/xlld/xll.d source/xlld/worksheet.d source/xlld/memorymanager.d source/xlld/memorypool.d source/xlld/xlcall.d source/xlld/xlcallcpp.d source/xlld/framework.d source/xlld/xl.d source/xlld/wrap.d source/xlld/test_d_funcs.d || goto :error
+dmd -m32 -c -ofmyxll.obj -debug -g -w -version=Have_d_excelsdk -Isource source\xlld\dummy.d source\xlld\framework.d source\xlld\memorymanager.d source\xlld\memorypool.d source\xlld\package.d source\xlld\test_d_funcs.d source\xlld\test_xl_funcs.d source\xlld\traits.d source\xlld\worksheet.d source\xlld\wrap.d source\xlld\xl.d source\xlld\xlcall.d source\xlld\xlcallcpp.d source\xlld\xll.d example\myxll.d -vcolumns
 
 rem Link the final XLL to be loaded by Excel
-dmd -m32 -ofmyxll32.xll -L/IMPLIB myxll.obj myxll32.def d_excelsdk.lib xlcall32d.lib -g -map || goto :error
+dmd -ofmyxll32.xll -L/IMPLIB myxll.obj myxll32.def d_excelsdk.lib xlcall32d.lib || goto :error
 
 echo.
 echo.
